1b13790fd1cab38392ffe026c253a05054cbec49
Enki Layman <layman212@gmail.com>
2023-05-15 19:18:09 -0400

    part 2 exercise 4

--- src/components/Course.js
+++ src/components/Course.js
@@ -1,16 +1,22 @@
 import React from 'react'
 
-const Course = ({course}) => {
-    console.log(course)
+const Course = ({courses}) => {
+    console.log(courses)
   return (
     <>
-        <h1>{course.name}</h1>
-        <ul>
-            {course.parts.map(c => {
-                return <li key={c.id}>{c.name} {c.exercises}</li>
-            })}
-        </ul>
-        <p><b>total of {course.parts.map(p => p.exercises).reduce((a, b) => a + b)} exercises.</b></p>
+        {courses.map(c => {
+            return (
+                <div key={c.id}>
+                    <h1>{c.name}</h1>
+                    <ul>
+                        {c.parts.map(p => { 
+                            return <li key={p.id}>{p.name} {p.exercises}</li>
+                        })}
+                    </ul>
+                    <p><b>total of {c.parts.map(p => p.exercises).reduce((a, b) => a + b)} exercises.</b></p>
+                </div>
+            )
+        })}
     </>
   )
 }
