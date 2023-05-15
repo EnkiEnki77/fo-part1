65937874af1ecc1164f14977a87119b095d05699
Enki Layman <layman212@gmail.com>
2023-05-14 23:44:28 -0400

    creating frontend for part 3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+const Note = (props) => {
+  return (
+    <li>
+       {props.note.content}
+    </li>
+  )
+}
 
-function App() {
+function App({notes}) {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note => <Note key={note.id} note={note}/>)}
+      </ul>
     </div>
   );
 }
