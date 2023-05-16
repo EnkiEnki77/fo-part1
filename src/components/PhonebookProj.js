a4a455b4730ec077bcf3c8f8383b9cffcb60921b
Enki Layman <layman212@gmail.com>
2023-05-15 20:46:20 -0400

    part 2 exercise 7

--- src/components/PhonebookProj.js
+++ src/components/PhonebookProj.js
@@ -2,16 +2,46 @@ import React, { useState } from 'react'
 
 const PhonebookProj = () => {
     const [persons, setPersons] = useState([
-        {name: 'Arto', id: 1}
+        {name: 'Arto', id: generateId()}
     ])
     const [newName, setNewName] = useState('')
+
+    function handlePhoneForm(event){
+        event.preventDefault()
+
+        const nameExists = persons.map(p => p.name).includes(newName)
+
+        console.log(nameExists)
+
+        if(nameExists == true){
+            return alert(`${newName} already exists in phonebook`)
+        }
+
+        const newPersonInfo = {
+            id: generateId(),
+            name: newName
+        }
+
+        setPersons(persons.concat(newPersonInfo))
+        setNewName('')
+    }
+
+    function handleNameInput(event){
+        setNewName(event.target.value)
+    }
+
+    function generateId(){
+        const id = Math.floor(Math.random() * 1000000000)
+
+        return id
+    }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={handlePhoneForm}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNameInput}/>
         </div>
         <div>
           <button type="submit">add</button>
