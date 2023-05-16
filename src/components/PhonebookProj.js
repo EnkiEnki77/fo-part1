3828474ad7b3c6e42890ced9b0be070dd9e742b2
Enki Layman <layman212@gmail.com>
2023-05-15 21:48:50 -0400

    part 2 exercise 9

--- src/components/PhonebookProj.js
+++ src/components/PhonebookProj.js
@@ -2,9 +2,14 @@ import React, { useState } from 'react'
 
 const PhonebookProj = () => {
     const [persons, setPersons] = useState([
-        {name: 'Arto', id: generateId()}
-    ])
+        { name: 'Arto Hellas', number: '040-123456', id: 1 },
+        { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+        { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+        { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+      ])
+    const [personsFiltered, setPersonsFiltered] = useState([])
     const [newPerson, setNewName] = useState({name: '', number: ''})
+    const [searchVal, setSearchVal] = useState('')
 
     function handlePhoneForm(event){
         event.preventDefault()
@@ -33,6 +38,15 @@ const PhonebookProj = () => {
         setNewName({...newPerson, [name]: value})
     }
 
+    function handleSearchInput(event){
+        const {value} = event.target
+        const filtered = persons.filter(p => p.name.toLowerCase() == value.toLowerCase())
+        console.log(filtered)
+
+        setSearchVal(value)
+        setPersonsFiltered(filtered)
+    }
+
     function generateId(){
         const id = Math.floor(Math.random() * 1000000000)
 
@@ -42,6 +56,10 @@ const PhonebookProj = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <label>
+        search: <input value={searchVal} onChange={handleSearchInput}/>
+      </label>
+      <h2>Add new</h2>
       <form onSubmit={handlePhoneForm}>
         <div>
           name: <input name='name' value={newPerson.name} onChange={handleNameInput}/>
@@ -52,11 +70,20 @@ const PhonebookProj = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      <ul>
-        {persons.map(p => {
-            return <li key={p.id}>{p.name}</li>
-        })}
-      </ul>
+      {searchVal.length == 0 ?
+        <ul>
+            {persons.map(p => {
+                return <li key={p.id}>{p.name} {p.number}</li>
+            })}
+        </ul>
+        :
+        <ul>
+            {personsFiltered.map(p => {
+                return <li key={p.id}>{p.name} {p.number}</li>
+            })}
+        </ul>
+      }
+      
     </div>
   )
 }
